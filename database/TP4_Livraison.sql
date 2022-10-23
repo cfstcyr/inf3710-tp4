83dd71034ffbfe007cc8490e78a08a7fe48ce2be
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-10-23 12:09:33 -0400

    feat: format queries

--- database/TP4_Livraison.sql
+++ database/TP4_Livraison.sql
@@ -111,91 +111,100 @@ CREATE TABLE IF NOT EXISTS TP4_Livraison.Contenir (
 
 -- POPULATE ==================================================
 
-INSERT INTO TP4_Livraison.Client VALUES (1, 'St-Cyr', 'Charles', 'charles@email.com', 'Rue Edouard-Monpetit', 'Montréal', 'A1A 1A1');
-INSERT INTO TP4_Livraison.Client VALUES (2, 'Brosseau', 'Mathilde', 'mathilde@email.com', 'Rue de Marseille', 'Montréal', 'B2B 2B2');
-INSERT INTO TP4_Livraison.Client VALUES (3, 'Noël', 'Père', 'papa@email.com', 'Rue Principale', 'Pôle Nord', 'H0H 0H0');
-INSERT INTO TP4_Livraison.Client VALUES (5, 'Dupuis', 'Isabelle', 'isabelle@email.com', 'Rue de Marseille', 'Montréal', 'B2B 2B2');
+--                                       id  nom         prenom      courriel                rue                     ville           codepostal
+INSERT INTO TP4_Livraison.Client VALUES (1, 'St-Cyr',   'Charles',  'charles@email.com',    'Rue Edouard-Monpetit', 'Montréal',     'A1A 1A1');
+INSERT INTO TP4_Livraison.Client VALUES (2, 'Brosseau', 'Mathilde', 'mathilde@email.com',   'Rue de Marseille',     'Montréal',     'B2B 2B2');
+INSERT INTO TP4_Livraison.Client VALUES (3, 'Noël',     'Père',     'papa@email.com',       'Rue Principale',       'Pôle Nord',    'H0H 0H0');
+INSERT INTO TP4_Livraison.Client VALUES (5, 'Dupuis',   'Isabelle', 'isabelle@email.com',   'Rue de Marseille',     'Montréal',     'B2B 2B2');
 
+--                                           numero           client
 INSERT INTO TP4_Livraison.Telephone VALUES ('(000) 111-2222', 1);
 INSERT INTO TP4_Livraison.Telephone VALUES ('(333) 444-5555', 2);
 INSERT INTO TP4_Livraison.Telephone VALUES ('(605) 313-4000', 3);
 
-INSERT INTO TP4_Livraison.Fournisseur VALUES (1, 'IGA Nourriture Inc', 'Rue de IGA');
-INSERT INTO TP4_Livraison.Fournisseur VALUES (2, 'RICARDO', 'Rue de Richard');
-INSERT INTO TP4_Livraison.Fournisseur VALUES (3, 'QC Transport', 'Rue du Québec');
-INSERT INTO TP4_Livraison.Fournisseur VALUES (4, 'Benjamin', 'Rue de Marseille Montréal B2B 2B2');
+--                                            id  nom                    adresse
+INSERT INTO TP4_Livraison.Fournisseur VALUES (1, 'IGA Nourriture Inc',  'Rue de IGA');
+INSERT INTO TP4_Livraison.Fournisseur VALUES (2, 'RICARDO',             'Rue de Richard');
+INSERT INTO TP4_Livraison.Fournisseur VALUES (3, 'QC Transport',        'Rue du Québec');
+INSERT INTO TP4_Livraison.Fournisseur VALUES (4, 'Benjamin',            'Rue de Marseille Montréal B2B 2B2');
 
-INSERT INTO TP4_Livraison.PlanRepas VALUES (1, 'végétarien', 2, 2, 150, 22.00, 1);
-INSERT INTO TP4_Livraison.PlanRepas VALUES (2, 'indien', 1, 4, 400, 44.50, 1);
-INSERT INTO TP4_Livraison.PlanRepas VALUES (3, 'japonais', 1, 4, 350, 52.50, 2);
-INSERT INTO TP4_Livraison.PlanRepas VALUES (4, 'français', 2, 2, 350, 26.25, 2);
-INSERT INTO TP4_Livraison.PlanRepas VALUES (5, 'cetogène', 3, 3, 80, 17.75, 2);
+--                                          id  categorie    fréquence  personnes   calories    prix    fournisseur
+INSERT INTO TP4_Livraison.PlanRepas VALUES (1, 'végétarien', 2,         2,          150,        22.00,  1);
+INSERT INTO TP4_Livraison.PlanRepas VALUES (2, 'indien',     1,         4,          400,        44.50,  1);
+INSERT INTO TP4_Livraison.PlanRepas VALUES (3, 'japonais',   1,         4,          350,        52.50,  2);
+INSERT INTO TP4_Livraison.PlanRepas VALUES (4, 'français',   2,         2,          350,        26.25,  2);
+INSERT INTO TP4_Livraison.PlanRepas VALUES (5, 'cetogène',   3,         3,          80,         17.75,  2);
 
-INSERT INTO TP4_Livraison.Abonner VALUES (1, 2, 4);
-INSERT INTO TP4_Livraison.Abonner VALUES (1, 1, 4);
-INSERT INTO TP4_Livraison.Abonner VALUES (1, 4, 4);
-INSERT INTO TP4_Livraison.Abonner VALUES (2, 1, 2);
-INSERT INTO TP4_Livraison.Abonner VALUES (2, 5, 15);
-INSERT INTO TP4_Livraison.Abonner VALUES (3, 4, 51); -- juste 51 semaines parce qu'à Noël il mange des biscuits
+--                                        client    plan   duree
+INSERT INTO TP4_Livraison.Abonner VALUES (1,        2,     4);
+INSERT INTO TP4_Livraison.Abonner VALUES (1,        1,     4);
+INSERT INTO TP4_Livraison.Abonner VALUES (1,        4,     4);
+INSERT INTO TP4_Livraison.Abonner VALUES (2,        1,     2);
+INSERT INTO TP4_Livraison.Abonner VALUES (2,        5,     15);
+INSERT INTO TP4_Livraison.Abonner VALUES (3,        4,     51); -- juste 51 semaines parce qu'à Noël il mange des biscuits
 
-INSERT INTO TP4_Livraison.KitRepas VALUES (1, 'Tofu général tao', 1); 
-INSERT INTO TP4_Livraison.KitRepas VALUES (2, 'Tacos végé au fromage fondant', 1);
-INSERT INTO TP4_Livraison.KitRepas VALUES (3, 'Poulet au beurre', 2);
-INSERT INTO TP4_Livraison.KitRepas VALUES (4, 'Brochette de porc Yakiton', 3);
-INSERT INTO TP4_Livraison.KitRepas VALUES (5, 'Bavette au beurre à léchalotte française', 4);
-INSERT INTO TP4_Livraison.KitRepas VALUES (6, 'Filets de turbot meunière', 4);
+--                                         id  nom                                          planrepas
+INSERT INTO TP4_Livraison.KitRepas VALUES (1, 'Tofu général tao',                           1); 
+INSERT INTO TP4_Livraison.KitRepas VALUES (2, 'Tacos végé au fromage fondant',              1);
+INSERT INTO TP4_Livraison.KitRepas VALUES (3, 'Poulet au beurre',                           2);
+INSERT INTO TP4_Livraison.KitRepas VALUES (4, 'Brochette de porc Yakiton',                  3);
+INSERT INTO TP4_Livraison.KitRepas VALUES (5, 'Bavette au beurre à léchalotte française',   4);
+INSERT INTO TP4_Livraison.KitRepas VALUES (6, 'Filets de turbot meunière',                  4);
 
-INSERT INTO TP4_Livraison.Image VALUES (1, 'https://images.ricardocuisine.com/services/recipes/5675.jpg', 1);
-INSERT INTO TP4_Livraison.Image VALUES (2, 'https://images.ricardocuisine.com/services/recipes/8049.jpg', 2);
+--                                      id  data                                                                            kitrepas
+INSERT INTO TP4_Livraison.Image VALUES (1, 'https://images.ricardocuisine.com/services/recipes/5675.jpg',                   1);
+INSERT INTO TP4_Livraison.Image VALUES (2, 'https://images.ricardocuisine.com/services/recipes/8049.jpg',                   2);
 INSERT INTO TP4_Livraison.Image VALUES (3, 'https://images.ricardocuisine.com/services/recipes/8504154235512b996db487.jpg', 3);
-INSERT INTO TP4_Livraison.Image VALUES (4, 'https://images.ricardocuisine.com/services/recipes/5788.jpg', 4);
-INSERT INTO TP4_Livraison.Image VALUES (5, 'https://images.ricardocuisine.com/services/recipes/8914.jpg', 5);
-INSERT INTO TP4_Livraison.Image VALUES (6, 'https://images.ricardocuisine.com/services/recipes/8286-portrait.jpg', 6);
+INSERT INTO TP4_Livraison.Image VALUES (4, 'https://images.ricardocuisine.com/services/recipes/5788.jpg',                   4);
+INSERT INTO TP4_Livraison.Image VALUES (5, 'https://images.ricardocuisine.com/services/recipes/8914.jpg',                   5);
+INSERT INTO TP4_Livraison.Image VALUES (6, 'https://images.ricardocuisine.com/services/recipes/8286-portrait.jpg',          6);
 
-INSERT INTO TP4_Livraison.Ingredient VALUES(1, 'Tofu ferme', 'Chine');
-INSERT INTO TP4_Livraison.Ingredient VALUES(2, 'Gingembre', 'Chine');
-INSERT INTO TP4_Livraison.Ingredient VALUES(3, 'Gousse dail', 'Inde');
-INSERT INTO TP4_Livraison.Ingredient VALUES(4, 'Poivrons verts', 'Canada');
-INSERT INTO TP4_Livraison.Ingredient VALUES(5, 'Tortillas', 'Mexique');
-INSERT INTO TP4_Livraison.Ingredient VALUES(6, 'Cheddar doux', 'États-Unis');
-INSERT INTO TP4_Livraison.Ingredient VALUES(7, 'Crème sure', 'Espagne');
-INSERT INTO TP4_Livraison.Ingredient VALUES(8, 'Jalapeño', 'Mexique');
-INSERT INTO TP4_Livraison.Ingredient VALUES(9, 'Quartiers de lime', 'Colombie');
-INSERT INTO TP4_Livraison.Ingredient VALUES(10, 'Haut de cuisses de poulet', 'États-Unis');
-INSERT INTO TP4_Livraison.Ingredient VALUES(11, 'Marinade au yogourt épicé', 'Canada');
-INSERT INTO TP4_Livraison.Ingredient VALUES(12, 'Sauce au beurre', 'Inde');
-INSERT INTO TP4_Livraison.Ingredient VALUES(13, 'Filet de porc', 'Canada');
-INSERT INTO TP4_Livraison.Ingredient VALUES(14, 'Oignons verts', 'Chine');
-INSERT INTO TP4_Livraison.Ingredient VALUES(15, 'Tranches de flanc de porc', 'Espagne');
-INSERT INTO TP4_Livraison.Ingredient VALUES(16, 'Graines de sésame', 'Japon');
-INSERT INTO TP4_Livraison.Ingredient VALUES(17, 'Échalote française', 'Canada');
-INSERT INTO TP4_Livraison.Ingredient VALUES(18, 'Bavette de boeuf', 'États-Unis');
-INSERT INTO TP4_Livraison.Ingredient VALUES(19, 'Beurre', 'Canada');
-INSERT INTO TP4_Livraison.Ingredient VALUES(20, 'Filets de turbot', 'Chine');
-INSERT INTO TP4_Livraison.Ingredient VALUES(21, 'Farine tout usage', 'Allemagne');
-INSERT INTO TP4_Livraison.Ingredient VALUES(22, 'Citron', 'Colombie');
+--                                          id  nom                              pays
+INSERT INTO TP4_Livraison.Ingredient VALUES(1, 'Tofu ferme',                    'Chine');
+INSERT INTO TP4_Livraison.Ingredient VALUES(2, 'Gingembre',                     'Chine');
+INSERT INTO TP4_Livraison.Ingredient VALUES(3, 'Gousse dail',                   'Inde');
+INSERT INTO TP4_Livraison.Ingredient VALUES(4, 'Poivrons verts',                'Canada');
+INSERT INTO TP4_Livraison.Ingredient VALUES(5, 'Tortillas',                     'Mexique');
+INSERT INTO TP4_Livraison.Ingredient VALUES(6, 'Cheddar doux',                  'États-Unis');
+INSERT INTO TP4_Livraison.Ingredient VALUES(7, 'Crème sure',                    'Espagne');
+INSERT INTO TP4_Livraison.Ingredient VALUES(8, 'Jalapeño',                      'Mexique');
+INSERT INTO TP4_Livraison.Ingredient VALUES(9, 'Quartiers de lime',             'Colombie');
+INSERT INTO TP4_Livraison.Ingredient VALUES(10, 'Haut de cuisses de poulet',    'États-Unis');
+INSERT INTO TP4_Livraison.Ingredient VALUES(11, 'Marinade au yogourt épicé',    'Canada');
+INSERT INTO TP4_Livraison.Ingredient VALUES(12, 'Sauce au beurre',              'Inde');
+INSERT INTO TP4_Livraison.Ingredient VALUES(13, 'Filet de porc',                'Canada');
+INSERT INTO TP4_Livraison.Ingredient VALUES(14, 'Oignons verts',                'Chine');
+INSERT INTO TP4_Livraison.Ingredient VALUES(15, 'Tranches de flanc de porc',    'Espagne');
+INSERT INTO TP4_Livraison.Ingredient VALUES(16, 'Graines de sésame',            'Japon');
+INSERT INTO TP4_Livraison.Ingredient VALUES(17, 'Échalote française',           'Canada');
+INSERT INTO TP4_Livraison.Ingredient VALUES(18, 'Bavette de boeuf',             'États-Unis');
+INSERT INTO TP4_Livraison.Ingredient VALUES(19, 'Beurre',                       'Canada');
+INSERT INTO TP4_Livraison.Ingredient VALUES(20, 'Filets de turbot',             'Chine');
+INSERT INTO TP4_Livraison.Ingredient VALUES(21, 'Farine tout usage',            'Allemagne');
+INSERT INTO TP4_Livraison.Ingredient VALUES(22, 'Citron',                       'Colombie');
 
-INSERT INTO TP4_Livraison.Contenir VALUES(1, 1);
-INSERT INTO TP4_Livraison.Contenir VALUES(1, 2);
-INSERT INTO TP4_Livraison.Contenir VALUES(1, 3);
-INSERT INTO TP4_Livraison.Contenir VALUES(2, 4);
-INSERT INTO TP4_Livraison.Contenir VALUES(2, 5);
-INSERT INTO TP4_Livraison.Contenir VALUES(2, 6);
-INSERT INTO TP4_Livraison.Contenir VALUES(2, 7);
-INSERT INTO TP4_Livraison.Contenir VALUES(2, 8);
-INSERT INTO TP4_Livraison.Contenir VALUES(2, 9);
-INSERT INTO TP4_Livraison.Contenir VALUES(3, 10);
-INSERT INTO TP4_Livraison.Contenir VALUES(3, 11);
-INSERT INTO TP4_Livraison.Contenir VALUES(3, 12);
-INSERT INTO TP4_Livraison.Contenir VALUES(4, 13);
-INSERT INTO TP4_Livraison.Contenir VALUES(4, 14);
-INSERT INTO TP4_Livraison.Contenir VALUES(4, 15);
-INSERT INTO TP4_Livraison.Contenir VALUES(4, 16);
-INSERT INTO TP4_Livraison.Contenir VALUES(4, 9);
-INSERT INTO TP4_Livraison.Contenir VALUES(5, 17);
-INSERT INTO TP4_Livraison.Contenir VALUES(5, 18);
-INSERT INTO TP4_Livraison.Contenir VALUES(5, 19);
-INSERT INTO TP4_Livraison.Contenir VALUES(6, 19);
-INSERT INTO TP4_Livraison.Contenir VALUES(6, 21);
-INSERT INTO TP4_Livraison.Contenir VALUES(6, 20);
-INSERT INTO TP4_Livraison.Contenir VALUES(6, 22);
+--                                        kitrepas  ingredient
+INSERT INTO TP4_Livraison.Contenir VALUES(1,        1);
+INSERT INTO TP4_Livraison.Contenir VALUES(1,        2);
+INSERT INTO TP4_Livraison.Contenir VALUES(1,        3);
+INSERT INTO TP4_Livraison.Contenir VALUES(2,        4);
+INSERT INTO TP4_Livraison.Contenir VALUES(2,        5);
+INSERT INTO TP4_Livraison.Contenir VALUES(2,        6);
+INSERT INTO TP4_Livraison.Contenir VALUES(2,        7);
+INSERT INTO TP4_Livraison.Contenir VALUES(2,        8);
+INSERT INTO TP4_Livraison.Contenir VALUES(2,        9);
+INSERT INTO TP4_Livraison.Contenir VALUES(3,        10);
+INSERT INTO TP4_Livraison.Contenir VALUES(3,        11);
+INSERT INTO TP4_Livraison.Contenir VALUES(3,        12);
+INSERT INTO TP4_Livraison.Contenir VALUES(4,        13);
+INSERT INTO TP4_Livraison.Contenir VALUES(4,        14);
+INSERT INTO TP4_Livraison.Contenir VALUES(4,        15);
+INSERT INTO TP4_Livraison.Contenir VALUES(4,        16);
+INSERT INTO TP4_Livraison.Contenir VALUES(4,        9);
+INSERT INTO TP4_Livraison.Contenir VALUES(5,        17);
+INSERT INTO TP4_Livraison.Contenir VALUES(5,        18);
+INSERT INTO TP4_Livraison.Contenir VALUES(5,        19);
+INSERT INTO TP4_Livraison.Contenir VALUES(6,        19);
+INSERT INTO TP4_Livraison.Contenir VALUES(6,        21);
+INSERT INTO TP4_Livraison.Contenir VALUES(6,        20);
+INSERT INTO TP4_Livraison.Contenir VALUES(6,        22);
