9a951ab34b59716815a5c4c8b63475af6d22775b
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-11-06 12:28:08 -0500

    feat: use table wrapper

--- client/src/components/table-wrapper/table-wrapper.component.ts
+++ client/src/components/table-wrapper/table-wrapper.component.ts
@@ -2,7 +2,9 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TableItem } from 'common/tables';
 import { Subject } from 'rxjs';
+import { AddPlanRepasComponent } from 'src/pages/add-plan-repas/add-plan-repas.component';
 import { DeleteTableItemComponent, DeleteTableItemData } from 'src/pages/delete-table-item/delete-table-item.component';
+import { UpdatePlanRepasComponent } from 'src/pages/update-plan-repas/update-plan-repas.component';
 import { DataService } from 'src/services/data-service/data.service';
 import { CollectionData, DefaultCollectionData } from 'src/utils/data';
 import { HelpersComponent } from '../helpers-component/helpers.component';
@@ -45,20 +47,38 @@ export class TableWrapperComponent<K extends keyof TableItem> extends HelpersCom
         table: this.table,
         item: item,
       },
-      width: 'calc(100% - 24px)',
+      width: 'calc(100% - 48px)',
       maxWidth: '450px',
+      maxHeight: 'calc(100vh - 48px)',  
     });
   }
 
   openUpdateScreen(data: TableItem[K]): void {
-    console.log('update', data);
-    // this.dialog.open(UpdatePlanRepasComponent, {
-    //   data: plan
-    // });
+    if (!this.table) throw new Error('Table is not set');
+
+    if (this.table === 'planRepas') {
+      this.dialog.open(UpdatePlanRepasComponent, {
+        data,
+        width: 'calc(100% - 48px)',
+        maxWidth: '450px',
+        maxHeight: 'calc(100vh - 48px)',  
+      });
+    } else {
+      alert(`Update ${this.table} not implemented`);
+    }
   }
 
   openAddScreen() {
-    console.log('add');
-    // this.dialog.open(AddPlanRepasComponent);
+    if (!this.table) throw new Error('Table is not set');
+
+    if (this.table === 'planRepas') {
+      this.dialog.open(AddPlanRepasComponent, {
+        width: 'calc(100% - 48px)',
+        maxWidth: '450px',
+        maxHeight: 'calc(100vh - 48px)',  
+      });
+    } else {
+      alert(`Update ${this.table} not implemented`);
+    }
   }
 }
--- client/src/pages/home/home.component.ts
+++ client/src/pages/home/home.component.ts
@@ -1,75 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/services/api-service/api.service';
-import { DataService } from 'src/services/data-service/data.service';
+import { Component } from '@angular/core';
 import { HelpersComponent } from 'src/components/helpers-component/helpers.component';
-import { Status } from 'common/communication/status';
-import { PlanRepas } from 'common/tables/plan-repas';
-import { DefaultCollectionData, CollectionData } from 'src/utils/data';
-import { Router } from '@angular/router';
-import { UpdatePlanRepasComponent } from '../update-plan-repas/update-plan-repas.component';
-import { MatDialog } from '@angular/material/dialog';
-import { AddPlanRepasComponent } from '../add-plan-repas/add-plan-repas.component';
-import { TABLE_ITEMS } from 'src/config/data';
-import { AlertManagerService } from 'src/services/alert-manager/alert-manager.service';
-import { DeleteTableItemComponent, DeleteTableItemData } from '../delete-table-item/delete-table-item.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent extends HelpersComponent implements OnInit {
-  protected planRepas: CollectionData<PlanRepas>;
-  protected tableItems = Object.values(TABLE_ITEMS);
-  public that = this;
-
-  constructor(
-    public dialog: MatDialog, 
-    private apiService: ApiService, 
-    protected dataService: DataService, 
-    private router: Router,
-    private alertManager: AlertManagerService,
-  ) {
-    super();
-    this.planRepas = DefaultCollectionData();
-  }
-
-  ngOnInit(): void {
-    this.dataService.subscribe('planRepas', (planRepas) => {
-      this.planRepas = planRepas;
-    });
-  }
-  
-  update(): void {
-    this.dataService.update('planRepas');
-  }
-  
-  openDeleteScreen(plan: PlanRepas): void {
-    this.dialog.open<unknown, DeleteTableItemData<'planRepas'>>(DeleteTableItemComponent, {
-      data: {
-        table: 'planRepas',
-        item: plan,
-      },
-      width: 'calc(100% - 48px)',
-      maxWidth: '450px',
-      maxHeight: 'calc(100vh - 48px)',
-    });
-  }
-
-  openUpdateScreen(plan: PlanRepas): void {
-    this.dialog.open(UpdatePlanRepasComponent, {
-      data: plan,
-      width: 'calc(100% - 48px)',
-      maxWidth: '450px',
-      maxHeight: 'calc(100vh - 48px)',
-    });
-  }
-
-  openAddScreen() {
-    this.dialog.open(AddPlanRepasComponent, {
-      width: 'calc(100% - 48px)',
-      maxWidth: '450px',
-      maxHeight: 'calc(100vh - 48px)',
-    });
-  }
-}
+export class HomeComponent extends HelpersComponent {}
