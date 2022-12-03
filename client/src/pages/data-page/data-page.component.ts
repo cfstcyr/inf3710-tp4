4849193d6ba620d1e4468a9d71d1bda94ec5eb5d
matbro-inextenso <matbro@inextenso.net>
2022-12-03 00:34:09 -0500

    Active and hoverred table is highlighted

--- client/src/pages/data-page/data-page.component.ts
+++ client/src/pages/data-page/data-page.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TableItem } from 'common/tables';
 import { HelpersComponent } from 'src/components/helpers-component/helpers.component';
-import { getTableFromPath, TABLE_ITEMS } from 'src/config/data';
+import { getTableFromPath, TableItemConfig, TABLE_ITEMS } from 'src/config/data';
+
+interface TableNameDisplay extends Omit<TableItemConfig<TableItem>, 'idKey'> {
+  active: boolean;
+}
 
 @Component({
   selector: 'app-data-page',
@@ -14,12 +18,34 @@ export class DataPageComponent<K extends keyof TableItem> extends HelpersCompone
   protected name?: string;
   protected tableItems = TABLE_ITEMS;
   protected tableNames: (keyof TableItem)[] = Object.keys(TABLE_ITEMS) as (keyof TableItem)[];
+  protected tableItemsHere: TableNameDisplay[];
 
   constructor(
     private readonly route: ActivatedRoute,
     private readonly router: Router,
   ) { 
     super();
+    
+    this.tableItemsHere = (Object.keys(this.tableItems) as (keyof TableItem)[]).map((item) => (
+        { 
+            path: TABLE_ITEMS[item]['path'], 
+            name: TABLE_ITEMS[item]['name'],
+            active: false 
+        }
+    ));
+    console.log(this.tableItemsHere[2].path);
+    console.log(this.router.isActive('data/' + this.tableItemsHere[2].path, { paths: 'subset', matrixParams: 'ignored', queryParams: 'ignored', fragment: 'ignored' }));
+    
+    
+
+    router.events.subscribe((event) => {
+        console.log('hihi');
+        console.log(this.router.isActive('data/' + this.tableItemsHere[2].path, { paths: 'subset', matrixParams: 'ignored', queryParams: 'ignored', fragment: 'ignored' }));
+        
+        this.tableItemsHere.forEach((item) => item.active = this.router.isActive('data/' + item.path, { paths: 'subset', matrixParams: 'ignored', queryParams: 'ignored', fragment: 'ignored' }));
+        console.log(this.tableItemsHere);
+    });
+    
   }
 
   ngOnInit(): void {
@@ -31,6 +57,10 @@ export class DataPageComponent<K extends keyof TableItem> extends HelpersCompone
         this.router.navigateByUrl('/404', { replaceUrl: true });
       }
     });
+    console.log(this.tableItems);
+    console.log(this.tableNames);
+    
+    
   }
 
 }
